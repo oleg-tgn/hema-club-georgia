97131c8fa8975924675cf1c1ab0111fa9aad37dd
Oleg Stelmakh <stelmah.taganrog@gmail.com>
2026-09-10 20:59:07 +0400

    Move WeaponsPanel component inside Hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,10 +1,85 @@
-import { getTranslations } from "next-intl/server";
 import CtaTile from "../ui/CtaTile";
 import Logo from "../icons/Logo";
-import WeaponsPanel from "./WeaponsPanel";
+import config from "@payload-config";
+import { getLocale, getTranslations } from "next-intl/server";
+import { getPayload } from "payload";
+import type { Locale } from "@/i18n/routing";
+import { weaponIcons } from "../icons/weapons";
+import { Weapon } from "@/payload-types";
+
+type TFunc = Awaited<ReturnType<typeof getTranslations>>;
+
+function DescriptionPanel({ t }: { t: TFunc }) {
+  return (
+    <>
+      <p className="text-base font-normal text-off-white sm:flex-1/2">
+        {t("description")}
+      </p>
+      <div className="flex flex-row h-10.5 gap-2 sm:h-22 sm:flex-1/2">
+        <CtaTile
+          href="#schedule"
+          className="flex-[4_0_0] text-xl flex sm:hidden"
+        >
+          {t.rich("ctaJoinMobile")}
+        </CtaTile>
+        <CtaTile
+          href="#schedule"
+          className="flex-[3_0_0] text-xl hidden sm:flex"
+        >
+          {t.rich("ctaJoin", { br: () => <br /> })}
+        </CtaTile>
+        <CtaTile href="#about" className="flex-[6_0_0] text-xl">
+          {t.rich("ctaSchedule")}
+        </CtaTile>
+      </div>
+    </>
+  );
+}
+
+function WeaponsPanel({ weapons }: { weapons: Weapon[] }) {
+  if (weapons.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-4 rounded-lg border border-off-white/30 bg-black/20 py-4 px-4 backdrop-blur-md">
+      <div className="flex divide-x divide-off-white/20">
+        {weapons.map((weapon) => {
+          const WeaponIcon = weaponIcons[weapon.slug];
+
+          return (
+            <div
+              key={weapon.id}
+              className="flex w-60 flex-col align-center gap-2 px-2 first:pl-0 last:pr-0"
+            >
+              <div className="flex h-20 w-full items-center justify-center">
+                {WeaponIcon && (
+                  <WeaponIcon className="h-full w-auto text-paper-100" />
+                )}
+              </div>
+              <span className="text-center font-serif text-3xl leading-8 font-light tracking-tight text-paper-100">
+                {weapon.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default async function Hero() {
   const t = await getTranslations("Hero");
+  const locale = await getLocale();
+  const payload = await getPayload({ config });
+
+  const { docs: weapons } = await payload.find({
+    collection: "weapons",
+    depth: 0,
+    limit: 10,
+    locale: locale as Locale,
+    sort: "order",
+  });
 
   return (
     <>
@@ -24,35 +99,16 @@ export default async function Hero() {
         </div>
 
         <div className="relative flex w-full items-end justify-between gap-10">
-          <div className="flex flex-col w-full gap-3 xl:max-w-91">
-            <p className="text-base font-normal text-off-white">
-              {t("description")}
-            </p>
-            <div className="flex flex-row h-10.5 sm:h-22 gap-2">
-              <CtaTile
-                href="#schedule"
-                className="flex-[4_0_0] text-xl flex sm:hidden"
-              >
-                {t.rich("ctaJoinMobile")}
-              </CtaTile>
-              <CtaTile
-                href="#schedule"
-                className="flex-[3_0_0] text-xl hidden sm:flex"
-              >
-                {t.rich("ctaJoin", { br: () => <br /> })}
-              </CtaTile>
-              <CtaTile href="#about" className="flex-[6_0_0] text-xl">
-                {t.rich("ctaSchedule")}
-              </CtaTile>
-            </div>
+          <div className="flex flex-col w-full gap-3 sm:flex-row xl:max-w-91">
+            <DescriptionPanel t={t} />
           </div>
           <div className="hidden portrait:sm:flex lg:flex">
-            <WeaponsPanel />
+            <WeaponsPanel weapons={weapons} />
           </div>
         </div>
       </section>
       <section className="flex portrait:sm:hidden lg:hidden">
-        <WeaponsPanel />
+        <WeaponsPanel weapons={weapons} />
       </section>
     </>
   );
