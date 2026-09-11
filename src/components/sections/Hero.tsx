fdac8769d3b4bcd53491a59e84ec456497c92cfa
Oleg Stelmakh <stelmah.taganrog@gmail.com>
2026-09-11 20:23:30 +0400

    Fix Hero weapons position for XL

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -44,14 +44,14 @@ function WeaponsPanel({ weapons }: { weapons: Weapon[] }) {
   }
 
   return (
-    <div className="flex flex-col w-full gap-2 md:gap-4 xl:flex-row">
+    <div className="flex flex-col w-full gap-2 md:gap-4 xl:flex-row xl:justify-end">
       {weapons.map((weapon) => {
         const WeaponIcon = weaponIcons[weapon.slug];
 
         return (
           <div
             key={weapon.id}
-            className="flex flex-col w-full align-center gap-2 p-4 rounded-[20px] bg-gold-100 border border-gold-100 sm:bg-transparent sm:backdrop-blur-md sm:gap-4 sm:border sm:border-off-white/30 sm:rounded-lg lg:flex-row lg:justify-between xl:flex-col"
+            className="flex flex-col w-full align-center gap-2 p-4 rounded-[20px] bg-gold-100 border border-gold-100 sm:bg-transparent sm:backdrop-blur-md sm:gap-4 sm:border sm:border-off-white/30 sm:rounded-lg lg:flex-row lg:justify-between xl:flex-col xl:max-w-59 2xl:max-w-68.5"
           >
             <div className="flex h-10.5 w-full lg:w-auto xl:w-full">
               {WeaponIcon && (
