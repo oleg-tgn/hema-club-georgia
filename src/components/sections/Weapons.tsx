83dbdfe3e6e1032033199064a0c20ae47db2a11f
Oleg Stelmakh <stelmah.taganrog@gmail.com>
2026-09-16 01:11:30 +0400

    Fix LG and XL left text width

--- src/components/sections/Weapons.tsx
+++ src/components/sections/Weapons.tsx
@@ -22,7 +22,7 @@ export default async function Weapons() {
         <WeaponsTitleIcon aria-hidden className="w-full text-gold-100/80" />
 
         <div className="flex w-full flex-col gap-6 sm:flex-row sm:gap-8 md:gap-8 lg:justify-between lg:-mt-10 xl:-mt-5 2xl:mt-12">
-          <div className="flex flex-col gap-6 w-full sm:w-auto md:flex-row md:justify-between lg:gap-12 lg:max-w-105 xl:flex-col xl:max-w-44 xl:gap-12.5 ">
+          <div className="flex flex-col gap-6 w-full sm:w-auto md:flex-row md:justify-between lg:gap-12 lg:max-w-41.5 xl:flex-col xl:gap-12.5 ">
             {weapons.provisions?.map((row) => (
               <div key={row.id}>
                 <div className="text-base font-normal">{row.intro}</div>
