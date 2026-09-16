7f8504b0e416df2a703a2465d032be3853358697
Oleg Stelmakh <stelmah.taganrog@gmail.com>
2026-09-16 13:49:08 +0400

    Fix Carousel margin and padding left

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -77,7 +77,7 @@ export function CarouselViewport({ children }: { children: ReactNode }) {
   const { viewportRef } = useCarouselContext();
   return (
     <div
-      className="full-bleed-inset w-screen ml-[calc(50%-50vw)] overflow-hidden select-none"
+      className="full-bleed-inset sm:w-screen sm:ml-[calc(50%-50vw)] overflow-hidden select-none"
       ref={viewportRef}
     >
       {children}
